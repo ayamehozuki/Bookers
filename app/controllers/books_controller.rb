ce7ff655b6245b715dd41515b9607e0bac331b17
EC2 Default User <ec2-user@ip-172-31-11-85.ap-northeast-1.compute.internal>
2024-10-17 06:44:42 +0000

    Initial Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,21 +1,19 @@
 class BooksController < ApplicationController
-  def new
+  
+  def index
     @book = Book.new
+    @books = Book.all
   end
   
-def create
-  @book = Book.new(book_params)
-  @books = Book.all
-  if @book.save
-    flash[:notice] = "Book was successfully created."
-    redirect_to book_path(@book)
-  else
-    render :index
-  end
-end
-
-  def index
+  def create
+    @book = Book.new(book_params)
     @books = Book.all
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      render :index
+    end
   end
 
   def show
